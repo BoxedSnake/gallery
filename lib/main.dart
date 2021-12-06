2c82641bcff1a09dd422355e54db4a3267d1855a
William <69453507+BoxedSnake@users.noreply.github.com>
2021-12-06 19:23:28 +0800

    progress added bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,131 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Gallery App')
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _RandomWordsState createState() => _RandomWordsState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Galleryaa'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.grid_view_outlined),
+          onPressed: (){},),
+          IconButton(
+            icon: Icon(Icons.sort_outlined),
+          onPressed: (){},),
+          IconButton(
+            icon: Icon(Icons.lock_outlined),
+          onPressed: (){},),
+
+          // IconButton(onPressed: _expandLayout, icon: icon)
+
+        ]
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Text("placeholder"),
+
+
+
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        color: Colors.black,
+        child: IconTheme(
+          data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+          child: Row(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            // Container(height: 50.0),
+            IconButton(
+              iconSize: 50,
+              onPressed: (){},
+              tooltip: 'Shows local photos',
+              padding: EdgeInsets.symmetric(horizontal: 100),
+              icon: const Icon(Icons.home_outlined),
+
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+
+            const Spacer(),
+
+            IconButton(
+              iconSize: 50,
+              onPressed: (){},
+              tooltip: 'Shows shared photos',
+              padding: EdgeInsets.only(right: 100),
+
+              icon: const Icon(Icons.people_outlined),
+
+            )
 
           ],
-        ),
+          ),
+        )
+
       ),
-
       floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Icon(Icons.add_a_photo_outlined)
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      // _buildSuggestions(),
 
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add_a_photo_outlined),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+
 
     );
+  }
+
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }//buildrow
+
+  Widget _buildSuggestions(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context,i) {
+        if(i.isOdd) return const Divider();
+        final index = i ~/ 2;
+        if (index >= _suggestions.length){
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+        },
+    );
+  } //buildsuggestion
+
+
+}
+
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+
+    return MaterialApp (
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+
+    );
+
 
 
   }

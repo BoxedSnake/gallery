481fa0e4fcf3acd0432375e2bd7694f688d883dd
BoxedSnake <williamte92@gmail.com>
2021-12-08 15:38:17 +0800

    bottombar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+
 void main() => runApp(MyApp());
 
 class RandomWords extends StatefulWidget {
@@ -13,80 +14,98 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
-  void _home =
+  var bodytext = Text("Hello");
 
+  // var
+  void _home() {
+    setState(() {
+      bodytext = Text("HomePage");
+    });
+  }
+
+  void _shared() {
+    setState(() {
+      bodytext = Text("Shared");
+    });
+  }
+
+  void _yupper() {
+    bodytext = Text("slkjdlfjldsfjldsx  ");
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Galleryaa'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.grid_view_outlined),
-          onPressed: (){},),
-          IconButton(
-            icon: Icon(Icons.sort_outlined),
-          onPressed: (){},),
-          IconButton(
-            icon: Icon(Icons.lock_outlined),
-          onPressed: (){},),
+      appBar: AppBar(title: const Text('Galleryaa'), actions: [
+        IconButton(
+          icon: Icon(Icons.grid_view_outlined),
+          onPressed: _yupper,
+        ),
+        IconButton(
+          icon: Icon(Icons.sort_outlined),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.lock_outlined),
+          onPressed: () {},
+        ),
 
-          // IconButton(onPressed: _expandLayout, icon: icon)
-
-        ]
-      ),
-      body: Text("placeholder"),
-
-
+        // IconButton(onPressed: _expandLayout, icon: icon)
+      ]),
+      body: bodytext,
 
       bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        color: Colors.black,
-        child: IconTheme(
-          data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
-          child: Row(
-          children: <Widget>[
-            // Container(height: 50.0),
-            IconButton(
-              iconSize: 50,
-              onPressed: (){},
-              tooltip: 'Shows local photos',
-              padding: EdgeInsets.symmetric(horizontal: 100),
-              icon: const Icon(Icons.home_outlined),
+          shape: const CircularNotchedRectangle(),
+          color: Colors.black,
+          child: IconTheme(
+            data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      onPressed: _home,
+                      tooltip: 'Shows local photos',
+                      color: Colors.white,
+                      icon: const Icon(Icons.home_outlined),
+                    ),
+                    const Text(
+                      "Home",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 10,
+                      ),
+                    )
+                  ],
+                ),
+                Column(mainAxisSize: MainAxisSize.min, children: [
+                  IconButton(
+                    onPressed: _shared,
+                    tooltip: 'Shows shared photos',
 
+                    icon: const Icon(Icons.people_outlined),
+                  ),
+                  const Text(
+                    "Shared",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                    ),
+                  )
+                ]
+                )
+              ],
             ),
-
-            const Spacer(),
-
-            IconButton(
-              iconSize: 50,
-              onPressed: (){},
-              tooltip: 'Shows shared photos',
-              padding: EdgeInsets.only(right: 100),
-
-              icon: const Icon(Icons.people_outlined),
-
-            )
-
-          ],
-          ),
-        )
-
+          )
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child: Icon(Icons.add_a_photo_outlined)
-      ),
+          onPressed: () {}, child: Icon(Icons.add_a_photo_outlined)),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       // _buildSuggestions(),
-
-
-
-
     );
   }
-
 
   Widget _buildRow(WordPair pair) {
     return ListTile(
@@ -95,39 +114,30 @@ class _RandomWordsState extends State<RandomWords> {
         style: _biggerFont,
       ),
     );
-  }//buildrow
+  } //buildrow
 
-  Widget _buildSuggestions(){
+  Widget _buildSuggestions() {
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context,i) {
-        if(i.isOdd) return const Divider();
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
         final index = i ~/ 2;
-        if (index >= _suggestions.length){
+        if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
         return _buildRow(_suggestions[index]);
-        },
+      },
     );
   } //buildsuggestion
 
-
 }
-
-
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-
-    return MaterialApp (
+    return MaterialApp(
       title: 'Startup Name Generator',
       home: RandomWords(),
-
     );
-
-
-
   }
 }

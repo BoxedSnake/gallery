f3f1b459a4dfec131a19c7112155a8e17fb57368
BoxedSnake <williamte92@gmail.com>
2021-12-15 14:39:10 +0800

    progress

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,32 @@ class RandomWords extends StatefulWidget {
   @override
   _RandomWordsState createState() => _RandomWordsState();
 }
+// enum viewStyle{gridthree,gridfive,list};
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  var _biggerFont =  TextStyle(fontSize: 18.0);
-  var bodytext  = Text("Hello");
+  var _biggerFont = TextStyle(fontSize: 18.0);
+  bool viewtype = true;
+  bool gridisthree = true;
 
 
+  void _toggleviewtype(){
+    setState(() {
+      if (viewtype) {
+        viewtype = false;
+      }
+      else{
+        viewtype = true;
+      }
+      // viewtype? false:true;
 
+    });
+  }
 
   void _home() {
     setState(() {
-      _biggerFont = TextStyle(fontSize: 10.0);
-      bodytext = Text("HomePage");
+      _biggerFont = TextStyle(fontSize: 15.0);
+
     });
   }
 
@@ -30,7 +43,6 @@ class _RandomWordsState extends State<RandomWords> {
     setState(() {
       _biggerFont = TextStyle(fontSize: 30.0);
 
-      bodytext = Text("Shared");
     });
   }
   //
@@ -42,10 +54,10 @@ class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Galleryaa'), actions: [
+      appBar: AppBar(title: const Text('Gallery'), actions: [
         IconButton(
           icon: Icon(Icons.grid_view_outlined),
-          onPressed: (){},
+          onPressed: _toggleviewtype,
         ),
         IconButton(
           icon: Icon(Icons.sort_outlined),
@@ -58,49 +70,10 @@ class _RandomWordsState extends State<RandomWords> {
 
         // IconButton(onPressed: _expandLayout, icon: icon)
       ]),
-      body: _buildSuggestions(),
+      body: _buildSuggestions(viewtype),
 
-      bottomNavigationBar: BottomAppBar(
-          shape: const CircularNotchedRectangle(),
-          color: Colors.black,
-          child: IconTheme(
-            data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
+      bottomNavigationBar: _bottomNavBar(),
 
-                      constraints: BoxConstraints(),
-                      onPressed: _home,
-                      tooltip: 'Shows local photos',
-                      color: Colors.white,
-                      icon: const Icon(Icons.home_outlined),
-                    ),
-                    const Text("Home", style: TextStyle(color: Colors.white, fontSize: 10),
-                    )
-                  ],
-                ),
-                Stack(
-                  // mainAxisSize: MainAxisSize.min,
-                  children: [
-                  IconButton(
-                    onPressed: _shared,
-                    tooltip: 'Shows shared photos',
-                    padding: EdgeInsets.zero,
-                    constraints: BoxConstraints(),
-                    icon: const Icon(Icons.people_outlined),
-                  ),
-                  const Text("Shared",style: TextStyle(color: Colors.white,fontSize: 10,),
-                  )
-                ],
-                )
-              ],
-            ),
-          )
-      ),
       floatingActionButton: FloatingActionButton(
           onPressed: () {}, child: Icon(Icons.add_a_photo_outlined)),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -108,61 +81,172 @@ class _RandomWordsState extends State<RandomWords> {
     );
   }
 
+
   Widget _buildTile(WordPair pair) {
     return Container(
-      //   child: ListTile(
-      //     title: Image.network('https://cdn.britannica.com/16/1016-050-8932B817/Gray-whale-breaching.jpg'),
-      //     // title: Text(pair.asPascalCase,style: _biggerFont),
-      //     trailing:Column(
-      //       children: [
-      //         IconButton(
-      //           onPressed: (){},
-      //           icon: Icon(Icons.more_vert),
-      //         ),
-      //         IconButton(
-      //             onPressed: (){},
-      //             icon: Icon(CupertinoIcons.heart))
-      //       ],
-      //     )
-      //   ),
-      // );
+        //   child: ListTile(
+        //     title: Image.network('https://cdn.britannica.com/16/1016-050-8932B817/Gray-whale-breaching.jpg'),
+        //     // title: Text(pair.asPascalCase,style: _biggerFont),
+        //     trailing:Column(
+        //       children: [
+        //         IconButton(
+        //           onPressed: (){},
+        //           icon: Icon(Icons.more_vert),
+        //         ),
+        //         IconButton(
+        //             onPressed: (){},
+        //             icon: Icon(CupertinoIcons.heart))
+        //       ],
+        //     )
+        //   ),
+        // );
         width: 420,
         height: 300,
         decoration: BoxDecoration(
             image: DecorationImage(
-              image: NetworkImage('https://cdn.britannica.com/16/1016-050-8932B817/Gray-whale-breaching.jpg'),
-              fit: BoxFit.cover,)
-        ),
-        child:Column(
+          image: NetworkImage(
+              'https://cdn.britannica.com/16/1016-050-8932B817/Gray-whale-breaching.jpg'),
+          fit: BoxFit.cover,
+        )),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             IconButton(
-              onPressed: (){},
+              onPressed: () {},
               icon: Icon(Icons.more_vert),
             ),
             Spacer(),
-            IconButton(
-                onPressed: (){},
-                icon: Icon(CupertinoIcons.heart))
+            IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.heart))
           ],
-        )
-
-    );
+        ));
   }
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return const Divider();
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
+
+  Widget _buildList(WordPair pair) {
+    bool isLiked = false;
+    Widget likedIcon(){
+      return isLiked? Icon(CupertinoIcons.heart):Icon(CupertinoIcons.heart_fill);
+    }
+    void toggleliked(){
+      setState(() {
+        if (isLiked) {
+          isLiked = false;
         }
-        return _buildTile(_suggestions[index]);
-      },
-    );
-  } //buildsuggestion
+        else{
+          isLiked = true;
+        }
+      }
+      );
+    };
+    
+
+
+
+        return ListTile(
+            leading: IconButton(onPressed: () {}, icon: likedIcon()),
+            title: Text(pair.asPascalCase,style: _biggerFont,),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_vert)
+              // Row(
+              // children:[
+              //   IconButton(
+              //     onPressed: () {},
+              //     icon: Icon(Icons.more_vert),
+              //   ),
+              //   IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.heart)
+              //   )
+              // ],
+            )
+        );
+      }
+      }
+
+          Widget _buildSuggestions(bool type) {
+        Widget gridview() {
+          return ListView.builder(
+            padding: const EdgeInsets.all(16.0),
+            itemBuilder: (context, i) {
+              if (i.isOdd) return const Divider();
+              final index = i ~/ 2;
+              if (index >= _suggestions.length) {
+                _suggestions.addAll(generateWordPairs().take(10));
+              }
+              return _buildTile(_suggestions[index]);
+            },
+          );
+        };
+
+
+        Widget listview() {
+          return ListView.builder(
+            padding: const EdgeInsets.all(16.0),
+            itemBuilder: (context, i) {
+              if (i.isOdd) return const Divider();
+              final index = i ~/ 2;
+              if (index >= _suggestions.length) {
+                _suggestions.addAll(generateWordPairs().take(10));
+              }
+              return _buildList(_suggestions[index]);
+            },
+          );
+        };
+
+        return type? gridview():listview();
+
+      } //buildsuggestion
+
+
+      Widget _bottomNavBar() {
+        return BottomAppBar(
+            shape: const CircularNotchedRectangle(),
+            color: Colors.black,
+            child: IconTheme(
+              data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        constraints: BoxConstraints(),
+                        onPressed: _home,
+                        tooltip: 'Shows local photos',
+                        color: Colors.white,
+                        icon: const Icon(Icons.home_outlined),
+                      ),
+                      const Text(
+                        "Home",
+                        style: TextStyle(color: Colors.white, fontSize: 10),
+                      )
+                    ],
+                  ),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: _shared,
+                        tooltip: 'Shows shared photos',
+                        padding: EdgeInsets.zero,
+                        constraints: BoxConstraints(),
+                        icon: const Icon(Icons.people_outlined),
+                      ),
+                      const Text(
+                        "Shared",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+        );
+      }
+    }
 
 }
 
